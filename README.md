4d0121b795042675dd6055ff9ef0750319f896d2
AryeshSaha <aryeshsaha5302@gmail.com>
2022-09-20 14:47:26 +0530

    final proj

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project-02-Web-Calculator
+Simple Calculator. Beginner level project using html css and js.
---
+++ index.js
@@ -0,0 +1,16 @@
+const display = (value) => {
+    document.getElementById('textarea').value += value;
+}
+
+const calculate = () => {
+    let value = document.getElementById('textarea').value;
+    let calculation = eval(value)
+    document.getElementById('textarea').value = calculation
+}
+
+const clr = () => {
+    document.getElementById('textarea').value = ''
+}
+
+// Project-02-Web-Calculator
+// Simple Web Calculator. Beginner level project using html css and js.
